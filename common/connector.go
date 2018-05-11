f615e1ffb66e1187106a764a0b074b9b4b98b38c
Vadim Chernov <dimuls@yandex.ru>
2018-05-11 20:19:02 +0300

    common: payment store interface with memory store implementation

--- common/connector.go
+++ common/connector.go
@@ -1,6 +1,10 @@
 package common
 
 import (
+	"errors"
+	"sync"
+	"time"
+
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/shopspring/decimal"
 )
@@ -123,4 +127,101 @@ type LightningConnector interface {
 	// FundsAvailable returns number of funds available under control of
 	// connector.
 	FundsAvailable() (decimal.Decimal, error)
+}
+
+// PaymentStore is used to store payments for external services (except
+// exchange, which is explicitly notified) for give them ability to
+// check whether payment is received.
+type PaymentsStore interface {
+	// Payment returns payment by id
+	Payment(id string) (*Payment, error)
+	// AddPayment add payment to the store
+	AddPayment(p *Payment) error
+}
+
+var PaymentNotFound = errors.New("payment not found")
+
+// MemoryPaymentsStore is a `PaymentStore` in-memory implementation,
+// which is able to periodically clean stored payments which are exceed
+// storing time.
+type MemoryPaymentsStore struct {
+	paymentsMutex    sync.RWMutex
+	payments         map[string]paymentWithDatetime
+	storingTime      time.Duration
+	cleanerStopper   chan struct{}
+	cleanerWaitGroup sync.WaitGroup
+}
+
+func NewMemoryPaymentsStore(storingTime time.Duration) *MemoryPaymentsStore {
+	return &MemoryPaymentsStore{
+		payments:    map[string]paymentWithDatetime{},
+		storingTime: storingTime,
+	}
+}
+
+type paymentWithDatetime struct {
+	*Payment
+	datetime time.Time
+}
+
+func (s *MemoryPaymentsStore) Payment(id string) (*Payment, error) {
+	s.paymentsMutex.RLock()
+	defer s.paymentsMutex.RUnlock()
+	p, exists := s.payments[id]
+	if !exists {
+		return nil, PaymentNotFound
+	}
+	return p.Payment, nil
+}
+
+func (s *MemoryPaymentsStore) AddPayment(p *Payment) error {
+	s.paymentsMutex.Lock()
+	defer s.paymentsMutex.Unlock()
+	s.payments[p.ID] = paymentWithDatetime{
+		Payment:  p,
+		datetime: time.Now(),
+	}
+	return nil
+}
+
+func (s *MemoryPaymentsStore) StartCleaner() error {
+	if s.cleanerStopper != nil {
+		return errors.New("already started")
+	}
+	s.cleanerWaitGroup.Add(1)
+	go func() {
+		defer s.cleanerWaitGroup.Done()
+		if s.cleanerStopper == nil {
+			return
+		}
+		for {
+			select {
+			case <-time.After(time.Minute):
+				s.cleanPayments()
+			case <-s.cleanerStopper:
+				return
+			}
+		}
+	}()
+	return nil
+}
+
+func (s *MemoryPaymentsStore) StopCleaner() error {
+	if s.cleanerStopper == nil {
+		return errors.New("not started yet or already stopped")
+	}
+	close(s.cleanerStopper)
+	s.cleanerWaitGroup.Wait()
+	s.cleanerStopper = nil
+	return nil
+}
+
+func (s *MemoryPaymentsStore) cleanPayments() {
+	s.paymentsMutex.Lock()
+	defer s.paymentsMutex.Unlock()
+	for id, p := range s.payments {
+		if p.datetime.Add(s.storingTime).Before(time.Now()) {
+			delete(s.payments, id)
+		}
+	}
 }
