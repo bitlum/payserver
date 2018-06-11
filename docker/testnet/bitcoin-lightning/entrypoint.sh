e03aa4a0c5988d8d290d35468ce6b8d16c26292b
Vadim Chernov <dimuls@yandex.ru>
2018-06-11 13:45:09 +0300

    docker: simnet and testnet dockers implementation
    Everythink is dockerized: blockchain nodes, lnd, connector and other
    auxiliary services.

---
+++ docker/testnet/bitcoin-lightning/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# This path is expected to be volume to make lnd data persistent.
+DATA_DIR=/root/.lnd
+
+# This path is expected to have default data used to init environment
+# at first deploy such as config.
+DEFAULTS_DIR=/root/default
+
+CONFIG=$DATA_DIR/lnd.conf
+
+# If data directory doesn't exists this means that volume is not mounted
+# or mounted incorrectly, so we must fail.
+if [ ! -d $DATA_DIR ]; then
+    echo "Data directory '$DATA_DIR' doesn't exists. Check your volume configuration."
+    exit 1
+fi
+
+# At first deploy config in datadir should not exists so we
+# copying it from default config shipped with docker.
+if [ ! -f $CONFIG ]; then
+    echo "Copying default config"
+    cp $DEFAULTS_DIR/lnd.conf $CONFIG
+fi
+
+# If external IP defined we need to set corresponding run option
+if [ ! -z "$EXTERNAL_IP" ]; then
+    EXTERNAL_IP_OPT="--externalip=$EXTERNAL_IP"
+fi
+
+lnd $EXTERNAL_IP_OPT
