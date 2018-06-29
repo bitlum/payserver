8394d5659acac44e12bf34280d3eefef45bf91d9
Vadim Chernov <dimuls@yandex.ru>
2018-06-29 17:58:36 +0300

    chain/net: handle `main` network name in `GetParams`

--- chains/net/net.go
+++ chains/net/net.go
@@ -12,43 +12,43 @@ func GetParams(asset string, netName string) (*chaincfg.Params, error) {
 	switch asset {
 	case "BTC":
 		switch netName {
-		case "mainnet":
+		case "mainnet", "main":
 			return &chaincfg.MainNetParams, nil
-		case "regtest","simnet":
+		case "regtest", "simnet":
 			return &chaincfg.RegressionNetParams, nil
-		case "testnet3","test","testnet":
+		case "testnet3", "test", "testnet":
 			return &chaincfg.TestNet3Params, nil
 		}
 
 	case "BCH":
 		switch netName {
-		case "mainnet":
+		case "mainnet", "main":
 			return &bitcoincash.MainNetParams, nil
-		case "regtest","simnet":
+		case "regtest", "simnet":
 			return &bitcoincash.RegressionNetParams, nil
-		case "testnet3","test","testnet":
+		case "testnet3", "test", "testnet":
 			return &bitcoincash.TestNet3Params, nil
 		}
 
 	case "LTC":
 		switch netName {
-		case "mainnet":
+		case "mainnet", "main":
 			return &litecoin.MainNetParams, nil
 		case "mainnet-legacy":
 			return &litecoin.MainNetParamsLegacy, nil
-		case "regtest","simnet":
+		case "regtest", "simnet":
 			return &litecoin.RegressionNetParams, nil
-		case "testnet4","test","testnet":
+		case "testnet4", "test", "testnet":
 			return &litecoin.TestNet4Params, nil
 		}
 
 	case "DASH":
 		switch netName {
-		case "mainnet":
+		case "mainnet", "main":
 			return &dash.MainNetParams, nil
-		case "regtest","simnet":
+		case "regtest", "simnet":
 			return &dash.RegressionNetParams, nil
-		case "testnet3","test","testnet":
+		case "testnet3", "test", "testnet":
 			return &dash.TestNet3Params, nil
 		}
 
@@ -57,6 +57,6 @@ func GetParams(asset string, netName string) (*chaincfg.Params, error) {
 			asset)
 	}
 
-	return nil, errors.Errorf("asset's network '%s' is " +
+	return nil, errors.Errorf("asset's network '%s' is "+
 		"invalid or unsupported", asset, netName)
 }
