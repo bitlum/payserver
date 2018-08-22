bee8e62a3ed3793c412619b9ea4bba39008f9491
Andrey Samokhvalov <andrew.shvv@gmail.com>
2018-08-22 19:53:30 +0300

    litecoin: fix validation

--- connectors/assets/litecoin/validation.go
+++ connectors/assets/litecoin/validation.go
@@ -9,18 +9,21 @@ import (
 // ValidateAddress ensures that address is valid and belongs to the given
 // network.
 func ValidateAddress(address, netName string) error {
-
 	netParams, err := GetParams(netName)
 	if err != nil {
 		return errors.Errorf("unable to get net params: %v", err)
 	}
 
-	// If there is error we shouldn't return it in mainnet straight away,
-	// but instead because there is a possibility of address belong to the
-	// legacy litecoin address type we should make another check.
 	err = validate(address, netParams)
 	if err != nil && netName != "mainnet" {
+		// If there is error we shouldn't return it in mainnet straight away,
+		// but instead because there is a possibility of address belong to the
+		// legacy litecoin address type we should make another check.
 		return err
+	} else if err == nil {
+		// If validation was successful, than address is valid and we should
+		// exit.
+		return nil
 	}
 
 	legacyNetParams, err := GetParams("mainnet-legacy")
