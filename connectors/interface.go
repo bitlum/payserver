9b737a4cbbb4a8df35d9e9ffd4aceb3a4d2bd2f6
Andrey Samokhvalov <andrew.shvv@gmail.com>
2018-08-22 20:04:58 +0300

    connctor: rework interfaces for new rpc

--- connectors/interface.go
+++ connectors/interface.go
@@ -13,56 +13,6 @@ type LightningInfo struct {
 	*lnrpc.GetInfoResponse
 }
 
-// GeneratedTransaction...
-type GeneratedTransaction interface {
-	// ID...
-	ID() string
-
-	// Bytes...
-	Bytes() []byte
-}
-
-type NetworkType string
-
-var (
-	Blockchain NetworkType = "blockchain"
-	Lightning  NetworkType = "lightning"
-)
-
-// Payment is the structure which describe the action of funds
-// movement from one User to another.
-type Payment struct {
-	// ID is an number which identifies the transaction inside the payment
-	// system.
-	ID string
-
-	// Amount is an number of money which is translated from one User to
-	// another in this transaction.
-	Amount decimal.Decimal
-
-	// Account is the receiver account.
-	Account string
-
-	// Address is an address of receiver.
-	Address string
-
-	// Type is a type of network which is used to deliver the payment.
-	Type NetworkType
-}
-
-// BlockchainPendingPayment is the transaction with confirmations number lower
-// than required by the payment system to treated them as confirmed.
-type BlockchainPendingPayment struct {
-	Payment
-
-	// Confirmations is the number of confirmations.
-	Confirmations int64
-
-	// ConfirmationsLeft is the number of confirmations left in order to
-	// interpret the transaction as confirmed.
-	ConfirmationsLeft int64
-}
-
 // BlockchainConnector is an interface which describes the blockchain service
 // which is able to connect to blockchain daemon of particular currency and
 // operate with transactions, addresses, and also  able to notify other
@@ -82,18 +32,15 @@ type BlockchainConnector interface {
 
 	// PendingTransactions return the transactions which has confirmation
 	// number lower the required by payment system.
-	PendingTransactions(account string) ([]*BlockchainPendingPayment, error)
+	PendingTransactions(account string) ([]*Payment, error)
 
-	// GenerateTransaction generates raw blockchain transaction.
-	GenerateTransaction(address string, amount string) (GeneratedTransaction, error)
+	// CreatePayment generates the payment, but not sends it,
+	// instead returns the payment id and waits for it to be approved.
+	CreatePayment(address, amount string) (*Payment, error)
 
-	// SendTransaction sends the given transaction to the blockchain network.
-	SendTransaction(rawTx []byte) error
-
-	// ReceivedPayments returns channel with transactions which are passed
-	// the minimum threshold required by the client to treat the
-	// transactions as confirmed.
-	ReceivedPayments() <-chan []*Payment
+	// SendPayment sends created previously payment to the
+	// blockchain network.
+	SendPayment(paymentID string) (*Payment, error)
 
 	// ValidateAddress takes the blockchain address and ensure its valid.
 	ValidateAddress(address string) error
@@ -112,11 +59,11 @@ type LightningConnector interface {
 	Info() (*LightningInfo, error)
 
 	// CreateInvoice is used to create lightning network invoice.
-	CreateInvoice(account string, amount string) (string, error)
+	CreateInvoice(account, amount, description string) (string, error)
 
 	// SendTo is used to send specific amount of money to address within this
 	// payment system.
-	SendTo(invoice string) error
+	SendTo(invoice, amount string) (*Payment, error)
 
 	// ConfirmedBalance return the amount of confirmed funds available for account.
 	// TODO(andrew.shvv) Implement lightning wallet balance
@@ -125,10 +72,6 @@ type LightningConnector interface {
 	// PendingBalance return the amount of funds waiting to be confirmed.
 	// TODO(andrew.shvv) Implement lightning wallet balance
 	PendingBalance(account string) (decimal.Decimal, error)
-
-	// ReceivedPayments returns channel with transactions which are passed
-	// the minimum threshold required by the client to treat as confirmed.
-	ReceivedPayments() <-chan *Payment
 
 	// QueryRoutes returns list of routes from to the given lnd node,
 	// and insures the the capacity of the channels is sufficient.
