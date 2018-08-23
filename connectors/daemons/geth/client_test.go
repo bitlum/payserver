1be4a5f4d9fdf1f9748519189f559d6f0ce73652
Andrey Samokhvalov <andrew.shvv@gmail.com>
2018-08-23 16:37:37 +0300

    connectors: fix tests

--- connectors/daemons/geth/client_test.go
+++ connectors/daemons/geth/client_test.go
@@ -6,10 +6,6 @@ import (
 
 	"math/big"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/onrik/ethrpc"
 )
 
@@ -61,7 +57,7 @@ func TestSignTransaction(t *testing.T) {
 		t.Fatalf("unable to unlock account: %v", err)
 	}
 
-	tx, rawTx, err := client.EthSignTransaction(ethrpc.T{
+	_, _, err = client.EthSignTransaction(ethrpc.T{
 		From:     address,
 		To:       address,
 		Gas:      21000,
@@ -73,26 +69,4 @@ func TestSignTransaction(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unable to sign tx: %v", err)
 	}
-
-	spew.Dump(tx)
-	spew.Dump(rawTx)
-}
-
-func TestTransactionDecode(t *testing.T) {
-
-	data := common.FromHex(
-		"0xf8658015825208948e643bc825bdb44dfecfdeebecbbe3f2a4d5d71b8203e88084687e1699a0224c8101372cb765e1e1c31607d149e6d06c73c97865e84de7ed37de931c5519a020ecd6fbb48368d36320967641d22703b0c649f0320fb78dce4218b6684667fb")
-
-	spew.Dump(data)
-
-	var tx types.Transaction
-	if err := rlp.DecodeBytes(data, &tx); err != nil {
-		t.Fatal(err)
-	}
-
-	spew.Dump(tx.ChainId(), tx)
-
-	chainID := big.NewInt(876546889)
-	signer := types.NewEIP155Signer(chainID)
-	spew.Dump(types.Sender(signer, &tx))
 }
