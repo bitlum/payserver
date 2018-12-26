ff182e02115223513ddfbf6706d15f303020e7cc
Andrey Samokhvalov <andrew.shvv@gmail.com>
2018-12-26 13:10:58 +0300

    docker: allow hub access bitcoind

--- docker/testnet/connector/entrypoint.sh
+++ docker/testnet/connector/entrypoint.sh
@@ -11,29 +11,29 @@ CONFIG=$DATA_DIR/connector.conf
 
 # If data directory doesn't exists this means that volume is not mounted
 # or mounted incorrectly, so we must fail.
-if [ ! -d $DATA_DIR ]; then
+if [[ ! -d ${DATA_DIR} ]]; then
     echo "Data directory '$DATA_DIR' doesn't exists. Check your volume configuration."
     exit 1
 fi
 
 # We always restoring default config shipped with docker.
 echo "Restoring default config"
-cp $DEFAULTS_DIR/connector.conf $CONFIG
+cp ${DEFAULTS_DIR}/connector.conf ${CONFIG}
 
 # We are using `exec` to enable gracefull shutdown of running daemon.
 # Check http://veithen.github.io/2014/11/16/sigterm-propagation.html.
 exec connector --config /root/.connector/connector.conf \
---bitcoin.user=$BITCOIN_RPC_USER \
---bitcoin.password=$BITCOIN_RPC_PASSWORD \
---bitcoin.forcelasthash=$CONNECTOR_BITCOIN_FORCE_HASH \
---bitcoincash.user=$BITCOIN_CASH_RPC_USER \
---bitcoincash.password=$BITCOIN_CASH_RPC_PASSWORD \
---bitcoincash.forcelasthash=$CONNECTOR_BITCOIN_CASH_FORCE_HASH \
---dash.user=$DASH_RPC_USER \
---dash.password=$DASH_RPC_PASSWORD \
---dash.forcelasthash=$CONNECTOR_DASH_FORCE_HASH \
---ethereum.password=$ETHEREUM_ACCOUNT_PASSWORD \
---litecoin.user=$LITECOIN_RPC_USER \
---litecoin.password=$LITECOIN_RPC_PASSWORD \
---litecoin.forcelasthash=$CONNECTOR_LITECOIN_FORCE_HASH \
---ethereum.forcelasthash=$CONNECTOR_ETHEREUM_FORCE_HASH
+--bitcoin.user=${BITCOIN_RPC_USER} \
+--bitcoin.password=${BITCOIN_RPC_PASSWORD} \
+--bitcoin.forcelasthash=${CONNECTOR_BITCOIN_FORCE_HASH} \
+--bitcoincash.user=${BITCOIN_CASH_RPC_USER} \
+--bitcoincash.password=${BITCOIN_CASH_RPC_PASSWORD} \
+--bitcoincash.forcelasthash=${CONNECTOR_BITCOIN_CASH_FORCE_HASH} \
+--dash.user=${DASH_RPC_USER} \
+--dash.password=${DASH_RPC_PASSWORD} \
+--dash.forcelasthash=${CONNECTOR_DASH_FORCE_HASH} \
+--ethereum.password=${ETHEREUM_ACCOUNT_PASSWORD} \
+--ethereum.forcelasthash=${CONNECTOR_ETHEREUM_FORCE_HASH}
+--litecoin.user=${LITECOIN_RPC_USER} \
+--litecoin.password=${LITECOIN_RPC_PASSWORD} \
+--litecoin.forcelasthash=${CONNECTOR_LITECOIN_FORCE_HASH}
